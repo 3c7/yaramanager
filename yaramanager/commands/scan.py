a8822f65e8310e0683f1cf47db9efb40e949c8ce
Nils.Kuhnert <Nils.Kuhnert@posteo.de>
2021-06-12 22:30:36 +0200

    Implements #1

--- yaramanager/commands/scan.py
+++ yaramanager/commands/scan.py
@@ -19,10 +19,11 @@ from yaramanager.utils.utils import filter_rules_by_name_and_tag, write_ruleset_
 @click.option("-T", "--timeout", default=0, type=int, help="Set timeout in seconds.")
 @click.option("-p", "--no-progress", is_flag=True, help="Disable progress bar.")
 @click.option("-c", "--csv", is_flag=True, help="CSV like output. Use together with -p!")
-@click.argument("file", type=click.Path(exists=True, file_okay=True, dir_okay=True), nargs=-1)
-def scan(tag: str, name: str, timeout: int, no_progress: bool, csv: bool, file: List[str]):
+@click.option("-r", "--recursive", is_flag=True, help="Scans directories recursively.")
+@click.argument("paths", type=click.Path(exists=True, file_okay=True, dir_okay=True), nargs=-1)
+def scan(tag: str, name: str, timeout: int, no_progress: bool, csv: bool, recursive: bool, paths: List[str]):
     c, ec = Console(), Console(stderr=True)
-    if len(file) == 0:
+    if len(paths) == 0:
         with click.Context(scan) as ctx:
             c.print(scan.get_help(ctx))
             exit(-1)
@@ -39,10 +40,15 @@ def scan(tag: str, name: str, timeout: int, no_progress: bool, csv: bool, file: 
     ruleset_compiled = yara.compile(ruleset_path)
     if not no_progress:
         c.print(f"Using ruleset {ruleset_path} for scanning. Check the rule file in case any error shows up.")
+    if recursive:
+        list_of_files = []
+        for path in paths:
+            list_of_files.extend(get_dir_recursive(path))
+        paths = list_of_files
     with Progress() if not no_progress else c as prog:
         if isinstance(prog, Progress):
-            t1 = prog.add_task("Scanning...", total=len(file))
-        for path in file:
+            t1 = prog.add_task("Scanning...", total=len(paths))
+        for path in paths:
             if isinstance(prog, Progress):
                 prog.update(t1, advance=1, description=f"Scanning {path}...")
             if os.path.isdir(path):
@@ -62,3 +68,14 @@ def scan(tag: str, name: str, timeout: int, no_progress: bool, csv: bool, file: 
         if isinstance(prog, Progress):
             prog.update(t1, description="Finished scanning!")
     os.remove(ruleset_path)
+
+
+def get_dir_recursive(path: str) -> List[str]:
+    """Grabbs all files from a given path recursively."""
+    files = []
+    for dir_entry in os.scandir(path):
+        if dir_entry.is_dir(follow_symlinks=True):
+            files.extend(get_dir_recursive(dir_entry))
+        else:
+            files.append(dir_entry.path)
+    return files
