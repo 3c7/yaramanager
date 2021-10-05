14977670cd090f9a5ca1a50d9e72885b3986fc51
Silvian I <silvian.iosub@gmail.com>
2021-10-05 18:44:35 +0200

    Add ruleset export functionality

--- yaramanager/commands/ruleset.py
+++ yaramanager/commands/ruleset.py
@@ -1,11 +1,15 @@
+import io
+
 import click
+import yara
 from rich.console import Console
 from rich.table import Table
 from yarabuilder import YaraBuilder
 from yaramanager.db.base import Ruleset
 from yaramanager.db.session import get_session
 from yaramanager.utils.utils import get_ruleset_by_identifier, rules_to_table
-
+from yaramanager.utils.utils import write_ruleset_to_tmp_file
+from pathlib import Path
 
 @click.group(help="Manage your rulesets")
 def ruleset():
@@ -52,6 +56,55 @@ def get(raw: bool, identifier: str):
         for rule in ruleset.rules:
             rule.add_to_yarabuilder(yb)
         c.print(yb.build_rules())
+
+
+@ruleset.command(help="Export all rules assigned to a ruleset.")
+@click.argument("identifier")
+@click.option("-s", "--single", is_flag=True, help="Write set of rules into a single yara file.")
+@click.option("-c", "--compiled", is_flag=True, help="Write compiled ruleset into a single file.")
+@click.argument("path", type=click.Path(dir_okay=True, file_okay=True, writable=True))
+def export(identifier: str, single: bool, compiled: bool, path: str):
+    c = Console()
+    session = get_session()
+    ruleset = get_ruleset_by_identifier(identifier, session)
+    if not ruleset:
+        c.print("Ruleset not found.")
+        exit(-1)
+
+    path = Path(path)
+    if len(ruleset.rules) == 0:
+        c.print(f"Found no matching rules.")
+        exit(-1)
+
+    if path.is_dir():
+        c.print(f"Given path ({path}) is a directory.")
+        exit(-1)
+
+    yb = YaraBuilder()
+    for rule in ruleset.rules:
+        try:
+            rule.add_to_yarabuilder(yb)
+        except ValueError as e:
+            ruleset.rules.remove(rule)
+            yb.yara_rules.popitem()
+            c.print(f"Error:{rule.name} not exported: {e}")
+
+    if single:
+        if path.suffix != ".yar":
+            path = Path(str(path) + ".yar")
+        with io.open(path, "w") as fh:
+            fh.write(yb.build_rules())
+
+    if compiled:
+        if path.suffix == ".yar":
+            path = Path(str(path)[:-4] + ".yac")
+        elif path.suffix == "":
+            path = Path(str(path) + ".yac")
+        tmp_path, size = write_ruleset_to_tmp_file(yb)
+        compiled: yara.Rules = yara.compile(tmp_path)
+        compiled.save(str(path))
+
+    c.print(f"Wrote {len(ruleset.rules)} rules.")
 
 @ruleset.command(help="Create a new ruleset.")
 @click.argument("name")
